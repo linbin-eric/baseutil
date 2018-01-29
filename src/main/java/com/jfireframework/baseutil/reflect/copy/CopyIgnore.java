c790ce44966daad6d38b143ce71c9b08499d3a5f
eric <eric@jfire.cn>
2018-01-29 22:25:30 +0800

    为ignore更新功能。

--- src/main/java/com/jfireframework/baseutil/reflect/copy/CopyIgnore.java
+++ src/main/java/com/jfireframework/baseutil/reflect/copy/CopyIgnore.java
@@ -1,10 +1,25 @@
 package com.jfireframework.baseutil.reflect.copy;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CopyIgnore
 {
-    
+	Class<?> from() default Object.class;
+	
+	Class<?> to() default Object.class;
+	
+	@Target({ METHOD, FIELD })
+	@Retention(RUNTIME)
+	@Documented
+	@interface List
+	{
+		CopyIgnore[] value();
+	}
 }
