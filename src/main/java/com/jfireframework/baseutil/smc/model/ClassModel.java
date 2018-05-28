80b1c35032093dc66c8761ba6549ddb1d0f795f1
eric <eric@jfire.cn>
2018-05-28 17:51:49 +0800

    在ClassModel中导入数据进行了更清晰的处理

--- src/main/java/com/jfireframework/baseutil/smc/model/ClassModel.java
+++ src/main/java/com/jfireframework/baseutil/smc/model/ClassModel.java
@@ -1,5 +1,6 @@
 package com.jfireframework.baseutil.smc.model;
 
+import java.lang.reflect.Method;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -11,163 +12,196 @@ import com.jfireframework.baseutil.smc.model.MethodModel.MethodModelKey;
 
 public class ClassModel
 {
-    private final String                     packageName  = "com.jfireframe.smc.output";
-    private final String                     className;
-    private Map<String, FieldModel>          fieldStore   = new HashMap<String, FieldModel>();
-    private Map<MethodModelKey, MethodModel> methodStore  = new HashMap<MethodModelKey, MethodModel>();
-    private Set<String>                      constructors = new HashSet<String>();
-    private Set<Class<?>>                    imports      = new HashSet<Class<?>>();
-    private Set<Class<?>>                    interfaces   = new HashSet<Class<?>>();
-    private Class<?>                         parentClass;
-    
-    public ClassModel(String className, Class<?> parentClass, Class<?>... interCc)
-    {
-        this.className = className;
-        this.parentClass = parentClass;
-        for (Class<?> each : interCc)
-        {
-            interfaces.add(each);
-        }
-    }
-    
-    private String buildClassDefinition()
-    {
-        StringCache cache = new StringCache();
-        if (parentClass == Object.class)
-        {
-            cache.append("public class ").append(className);
-        }
-        else
-        {
-            cache.append("public class ").append(className).append(" extends ").append(SmcHelper.getTypeName(parentClass));
-        }
-        if (interfaces.isEmpty() == false)
-        {
-            cache.append(" implements ");//
-            for (Class<?> each : interfaces)
-            {
-                cache.append(SmcHelper.getTypeName(each)).appendComma();
-            }
-            if (cache.isCommaLast())
-            {
-                cache.deleteLast();
-            }
-        }
-        cache.append(" \r\n{\r\n");
-        return cache.toString();
-    }
-    
-    public void addInterface(Class<?> intercc)
-    {
-        interfaces.add(intercc);
-    }
-    
-    public void addImport(Class<?>... ckasses)
-    {
-        for (Class<?> each : ckasses)
-        {
-            imports.add(each);
-        }
-    }
-    
-    public void addConstructor(String initStr, Class<?>... params)
-    {
-        StringCache cache = new StringCache();
-        cache.append("public ").append(className);
-        if (params.length == 0)
-        {
-            cache.append("()\r\n{");
-        }
-        else
-        {
-            cache.append("(");
-            for (int i = 0; i < params.length; i++)
-            {
-                cache.append(SmcHelper.getTypeName(params[i])).append(" ");
-                cache.append("$").append(i).append(",");
-            }
-            cache.deleteLast().append(")\r\n{");
-        }
-        cache.append(initStr).append("}\r\n");
-        constructors.add(cache.toString());
-    }
-    
-    public void putMethodModel(MethodModel methodModel)
-    {
-        methodStore.put(methodModel.generateKey(), methodModel);
-    }
-    
-    public MethodModel getMethodModel(MethodModelKey key)
-    {
-        return methodStore.get(key);
-    }
-    
-    public MethodModel removeMethodModel(MethodModelKey key)
-    {
-        return methodStore.remove(key);
-    }
-    
-    public String fileName()
-    {
-        return className + ".java";
-    }
-    
-    public String className()
-    {
-        return className;
-    }
-    
-    public void addField(FieldModel... models)
-    {
-        for (FieldModel each : models)
-        {
-            fieldStore.put(each.getName(), each);
-        }
-    }
-    
-    public Collection<MethodModelKey> methods()
-    {
-        return methodStore.keySet();
-    }
-    
-    @Override
-    public String toString()
-    {
-        StringCache cache = new StringCache();
-        cache.append("package ").append(packageName).append(';').append("\r\n");
-        for (Class<?> each : imports)
-        {
-            cache.append("import ").append(SmcHelper.getTypeName(each)).append(";\r\n");
-        }
-        cache.append(buildClassDefinition());
-        for (String constructor : constructors)
-        {
-            cache.append('\t').append(constructor);
-        }
-        for (FieldModel each : fieldStore.values())
-        {
-            cache.append('\t').append(each.toString());
-        }
-        for (MethodModel each : methodStore.values())
-        {
-            cache.append('\t').append(each.toString());
-        }
-        cache.append("}");
-        return cache.toString();
-    }
-    
-    public String toStringWithLineNo()
-    {
-        String source = toString();
-        String[] tmp = source.split("\r\n");
-        StringCache cache = new StringCache(source.length());
-        int no = 1;
-        for (String each : tmp)
-        {
-            cache.append("/*").append(no).append("*/").append(each).append("\r\n");
-            no += 1;
-        }
-        return cache.toString();
-    }
-    
+	private final String						packageName		= "com.jfireframe.smc.output";
+	private final String						className;
+	private Map<String, FieldModel>				fieldStore		= new HashMap<String, FieldModel>();
+	private Map<MethodModelKey, MethodModel>	methodStore		= new HashMap<MethodModelKey, MethodModel>();
+	private Set<String>							constructors	= new HashSet<String>();
+	private Set<Class<?>>						imports			= new HashSet<Class<?>>();
+	private Set<Class<?>>						interfaces		= new HashSet<Class<?>>();
+	private Class<?>							parentClass;
+	
+	public ClassModel(String className, Class<?> parentClass, Class<?>... interCc)
+	{
+		this.className = className;
+		this.parentClass = parentClass;
+		for (Class<?> each : interCc)
+		{
+			interfaces.add(each);
+		}
+	}
+	
+	private String buildClassDefinition()
+	{
+		StringCache cache = new StringCache();
+		if (parentClass == Object.class)
+		{
+			cache.append("public class ").append(className);
+		}
+		else
+		{
+			cache.append("public class ").append(className).append(" extends ").append(SmcHelper.getTypeName(parentClass));
+		}
+		if (interfaces.isEmpty() == false)
+		{
+			cache.append(" implements ");//
+			for (Class<?> each : interfaces)
+			{
+				cache.append(SmcHelper.getTypeName(each)).appendComma();
+			}
+			if (cache.isCommaLast())
+			{
+				cache.deleteLast();
+			}
+		}
+		cache.append(" \r\n{\r\n");
+		return cache.toString();
+	}
+	
+	public void addInterface(Class<?> intercc)
+	{
+		imports.add(intercc);
+		interfaces.add(intercc);
+	}
+	
+	public void addImport(Class<?>... ckasses)
+	{
+		for (Class<?> each : ckasses)
+		{
+			for (Method method : each.getMethods())
+			{
+				addImportFilterArrayAndPrimitive(method.getReturnType());
+				for (Class<?> ckass : method.getParameterTypes())
+				{
+					addImportFilterArrayAndPrimitive(ckass);
+				}
+			}
+			addImportFilterArrayAndPrimitive(each);
+		}
+	}
+	
+	private void addImportFilterArrayAndPrimitive(Class<?> type)
+	{
+		if (type.isPrimitive())
+		{
+			return;
+		}
+		if (type.isArray())
+		{
+			while (type.isArray())
+			{
+				type = type.getComponentType();
+			}
+			if (type.isPrimitive() == false)
+			{
+				imports.add(type);
+			}
+		}
+		else
+		{
+			imports.add(type);
+		}
+	}
+	
+	public void addConstructor(String initStr, Class<?>... params)
+	{
+		StringCache cache = new StringCache();
+		cache.append("public ").append(className);
+		if (params.length == 0)
+		{
+			cache.append("()\r\n{");
+		}
+		else
+		{
+			cache.append("(");
+			for (int i = 0; i < params.length; i++)
+			{
+				cache.append(SmcHelper.getTypeName(params[i])).append(" ");
+				cache.append("$").append(i).append(",");
+			}
+			cache.deleteLast().append(")\r\n{");
+		}
+		cache.append(initStr).append("}\r\n");
+		constructors.add(cache.toString());
+	}
+	
+	public void putMethodModel(MethodModel methodModel)
+	{
+		methodStore.put(methodModel.generateKey(), methodModel);
+	}
+	
+	public MethodModel getMethodModel(MethodModelKey key)
+	{
+		return methodStore.get(key);
+	}
+	
+	public MethodModel removeMethodModel(MethodModelKey key)
+	{
+		return methodStore.remove(key);
+	}
+	
+	public String fileName()
+	{
+		return className + ".java";
+	}
+	
+	public String className()
+	{
+		return className;
+	}
+	
+	public void addField(FieldModel... models)
+	{
+		for (FieldModel each : models)
+		{
+			fieldStore.put(each.getName(), each);
+			addImport(each.getType());
+		}
+	}
+	
+	public Collection<MethodModelKey> methods()
+	{
+		return methodStore.keySet();
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringCache cache = new StringCache();
+		cache.append("package ").append(packageName).append(';').append("\r\n");
+		for (Class<?> each : imports)
+		{
+			cache.append("import ").append(SmcHelper.getTypeName(each)).append(";\r\n");
+		}
+		cache.append(buildClassDefinition());
+		for (String constructor : constructors)
+		{
+			cache.append('\t').append(constructor);
+		}
+		for (FieldModel each : fieldStore.values())
+		{
+			cache.append('\t').append(each.toString());
+		}
+		for (MethodModel each : methodStore.values())
+		{
+			cache.append('\t').append(each.toString());
+		}
+		cache.append("}");
+		return cache.toString();
+	}
+	
+	public String toStringWithLineNo()
+	{
+		String source = toString();
+		String[] tmp = source.split("\r\n");
+		StringCache cache = new StringCache(source.length());
+		int no = 1;
+		for (String each : tmp)
+		{
+			cache.append("/*").append(no).append("*/").append(each).append("\r\n");
+			no += 1;
+		}
+		return cache.toString();
+	}
+	
 }
--- src/main/java/com/jfireframework/baseutil/smc/model/MethodModel.java
+++ src/main/java/com/jfireframework/baseutil/smc/model/MethodModel.java
@@ -7,315 +7,336 @@ import com.jfireframework.baseutil.smc.SmcHelper;
 
 public class MethodModel
 {
-    public static enum AccessLevel
-    {
-        PUBLIC, PRIVATE, PROTECTED
-    }
-    
-    private AccessLevel accessLevel;
-    private Class<?>    returnType;
-    private Class<?>[]  paramterTypes;
-    // 该数组为空时意味着全部属性都不需要用final修饰
-    private boolean[]   paramterFinals;
-    private Class<?>[]  throwables;
-    private String      methodName;
-    private String      body;
-    
-    public MethodModel()
-    {
-    }
-    
-    public MethodModel(Method method)
-    {
-        int modifiers = method.getModifiers();
-        if (Modifier.isPublic(modifiers))
-        {
-            accessLevel = AccessLevel.PUBLIC;
-        }
-        else if (Modifier.isPrivate(modifiers))
-        {
-            accessLevel = AccessLevel.PRIVATE;
-        }
-        else if (Modifier.isProtected(modifiers))
-        {
-            accessLevel = AccessLevel.PROTECTED;
-        }
-        methodName = method.getName();
-        returnType = method.getReturnType();
-        paramterTypes = method.getParameterTypes();
-        throwables = method.getExceptionTypes();
-    }
-    
-    public MethodModel(MethodModel methodModel)
-    {
-        methodName = methodModel.methodName;
-        accessLevel = methodModel.accessLevel;
-        paramterTypes = methodModel.paramterTypes;
-        throwables = methodModel.throwables;
-        returnType = methodModel.returnType;
-    }
-    
-    @Override
-    public String toString()
-    {
-        StringCache cache = new StringCache();
-        switch (accessLevel)
-        {
-            case PUBLIC:
-                cache.append("public ");
-                break;
-            case PRIVATE:
-                cache.append("private ");
-                break;
-            case PROTECTED:
-                cache.append("protected ");
-                break;
-            default:
-                cache.append("public ");
-                break;
-        }
-        cache.append(SmcHelper.getTypeName(returnType)).append(' ')//
-                .append(methodName).append('(');
-        if (paramterTypes != null && paramterTypes.length > 0)
-        {
-            if (paramterFinals == null || paramterFinals.length == 0)
-            {
-                for (int i = 0; i < paramterTypes.length; i++)
-                {
-                    Class<?> each = paramterTypes[i];
-                    cache.append(SmcHelper.getTypeName(each)).append(" $").append(i).appendComma();
-                }
-            }
-            else
-            {
-                for (int i = 0; i < paramterTypes.length; i++)
-                {
-                    Class<?> each = paramterTypes[i];
-                    if (paramterFinals[i])
-                    {
-                        cache.append("final ");
-                    }
-                    cache.append(SmcHelper.getTypeName(each)).append(" $").append(i).appendComma();
-                }
-            }
-            if (cache.isCommaLast())
-            {
-                cache.deleteLast();
-            }
-        }
-        cache.append(')');
-        if (throwables != null && throwables.length != 0)
-        {
-            cache.append(" throws ");
-            for (Class<?> each : throwables)
-            {
-                cache.append(SmcHelper.getTypeName(each)).appendComma();
-            }
-            cache.deleteLast();
-        }
-        cache.append(" \r\n\t{\r\n");//
-        String[] tmp = body.split("\r\n");
-        for (String each : tmp)
-        {
-            cache.append("\t\t").append(each).append("\r\n");
-        }
-        cache.append("\t}\r\n");
-        return cache.toString();
-    }
-    
-    @Override
-    public int hashCode()
-    {
-        return methodName.hashCode();
-    }
-    
-    @Override
-    public boolean equals(Object o)
-    {
-        if (o instanceof MethodModel == false)
-        {
-            return false;
-        }
-        MethodModel target = (MethodModel) o;
-        if (accessLevel != target.accessLevel)
-        {
-            return false;
-        }
-        if (methodName.equals(target.methodName) == false)
-        {
-            return false;
-        }
-        if (paramterTypes.length != target.paramterTypes.length)
-        {
-            return false;
-        }
-        for (int i = 0; i < paramterTypes.length; i++)
-        {
-            Class<?> class1 = paramterTypes[i];
-            Class<?> class2 = target.paramterTypes[i];
-            if (class1 != class2)
-            {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    public MethodModelKey generateKey()
-    {
-        MethodModelKey key = new MethodModelKey();
-        key.accessLevel = accessLevel;
-        key.methodName = methodName;
-        key.paramterTypes = paramterTypes;
-        return key;
-    }
-    
-    public static class MethodModelKey
-    {
-        private AccessLevel accessLevel;
-        private Class<?>[]  paramterTypes;
-        private String      methodName;
-        
-        public MethodModelKey()
-        {
-        }
-        
-        public MethodModelKey(Method method)
-        {
-            int modifiers = method.getModifiers();
-            if (Modifier.isPublic(modifiers))
-            {
-                accessLevel = AccessLevel.PUBLIC;
-            }
-            else if (Modifier.isPrivate(modifiers))
-            {
-                accessLevel = AccessLevel.PRIVATE;
-            }
-            else if (Modifier.isProtected(modifiers))
-            {
-                accessLevel = AccessLevel.PROTECTED;
-            }
-            methodName = method.getName();
-            paramterTypes = method.getParameterTypes();
-        }
-        
-        @Override
-        public int hashCode()
-        {
-            return methodName.hashCode();
-        }
-        
-        @Override
-        public boolean equals(Object o)
-        {
-            if (o instanceof MethodModel == false)
-            {
-                return false;
-            }
-            MethodModel target = (MethodModel) o;
-            if (accessLevel != target.accessLevel)
-            {
-                return false;
-            }
-            if (methodName.equals(target.methodName) == false)
-            {
-                return false;
-            }
-            if (paramterTypes.length != target.paramterTypes.length)
-            {
-                return false;
-            }
-            for (int i = 0; i < paramterTypes.length; i++)
-            {
-                Class<?> class1 = paramterTypes[i];
-                Class<?> class2 = target.paramterTypes[i];
-                if (class1 != class2)
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
-        
-        public void setAccessLevel(AccessLevel accessLevel)
-        {
-            this.accessLevel = accessLevel;
-        }
-        
-        public void setParamterTypes(Class<?>[] paramterTypes)
-        {
-            this.paramterTypes = paramterTypes;
-        }
-        
-        public void setMethodName(String methodName)
-        {
-            this.methodName = methodName;
-        }
-        
-    }
-    
-    public AccessLevel getAccessLevel()
-    {
-        return accessLevel;
-    }
-    
-    public void setAccessLevel(AccessLevel accessLevel)
-    {
-        this.accessLevel = accessLevel;
-    }
-    
-    public Class<?> getReturnType()
-    {
-        return returnType;
-    }
-    
-    public void setReturnType(Class<?> returnType)
-    {
-        this.returnType = returnType;
-    }
-    
-    public Class<?>[] getParamterTypes()
-    {
-        return paramterTypes;
-    }
-    
-    public void setParamterTypes(Class<?>... paramterTypes)
-    {
-        this.paramterTypes = paramterTypes;
-    }
-    
-    public Class<?>[] getThrowables()
-    {
-        return throwables;
-    }
-    
-    public void setThrowables(Class<?>... throwables)
-    {
-        this.throwables = throwables;
-    }
-    
-    public String getMethodName()
-    {
-        return methodName;
-    }
-    
-    public void setMethodName(String methodName)
-    {
-        this.methodName = methodName;
-    }
-    
-    public String getBody()
-    {
-        return body;
-    }
-    
-    public void setBody(String body)
-    {
-        this.body = body;
-    }
-    
-    public void setParamterFinals(boolean... paramterFinals)
-    {
-        this.paramterFinals = paramterFinals;
-    }
+	public static enum AccessLevel
+	{
+		PUBLIC, PRIVATE, PROTECTED
+	}
+	
+	private AccessLevel	accessLevel;
+	private Class<?>	returnType;
+	private Class<?>[]	paramterTypes;
+	// 该数组为空时意味着全部属性都不需要用final修饰
+	private boolean[]	paramterFinals;
+	private Class<?>[]	throwables;
+	private String		methodName;
+	private String		body;
+	
+	public MethodModel()
+	{
+	}
+	
+	public MethodModel(Method method)
+	{
+		int modifiers = method.getModifiers();
+		if (Modifier.isPublic(modifiers))
+		{
+			accessLevel = AccessLevel.PUBLIC;
+		}
+		else if (Modifier.isPrivate(modifiers))
+		{
+			accessLevel = AccessLevel.PRIVATE;
+		}
+		else if (Modifier.isProtected(modifiers))
+		{
+			accessLevel = AccessLevel.PROTECTED;
+		}
+		methodName = method.getName();
+		returnType = method.getReturnType();
+		paramterTypes = method.getParameterTypes();
+		throwables = method.getExceptionTypes();
+	}
+	
+	public MethodModel(MethodModel methodModel)
+	{
+		methodName = methodModel.methodName;
+		accessLevel = methodModel.accessLevel;
+		paramterTypes = methodModel.paramterTypes;
+		throwables = methodModel.throwables;
+		returnType = methodModel.returnType;
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringCache cache = new StringCache();
+		switch (accessLevel)
+		{
+			case PUBLIC:
+				cache.append("public ");
+				break;
+			case PRIVATE:
+				cache.append("private ");
+				break;
+			case PROTECTED:
+				cache.append("protected ");
+				break;
+			default:
+				cache.append("public ");
+				break;
+		}
+		cache.append(SmcHelper.getTypeName(returnType)).append(' ')//
+		        .append(methodName).append('(');
+		if (paramterTypes != null && paramterTypes.length > 0)
+		{
+			if (paramterFinals == null || paramterFinals.length == 0)
+			{
+				for (int i = 0; i < paramterTypes.length; i++)
+				{
+					Class<?> each = paramterTypes[i];
+					cache.append(SmcHelper.getTypeName(each)).append(" $").append(i).appendComma();
+				}
+			}
+			else
+			{
+				for (int i = 0; i < paramterTypes.length; i++)
+				{
+					Class<?> each = paramterTypes[i];
+					if (paramterFinals[i])
+					{
+						cache.append("final ");
+					}
+					cache.append(SmcHelper.getTypeName(each)).append(" $").append(i).appendComma();
+				}
+			}
+			if (cache.isCommaLast())
+			{
+				cache.deleteLast();
+			}
+		}
+		cache.append(')');
+		if (throwables != null && throwables.length != 0)
+		{
+			cache.append(" throws ");
+			for (Class<?> each : throwables)
+			{
+				cache.append(SmcHelper.getTypeName(each)).appendComma();
+			}
+			cache.deleteLast();
+		}
+		cache.append(" \r\n\t{\r\n");//
+		String[] tmp = body.split("\r\n");
+		for (String each : tmp)
+		{
+			cache.append("\t\t").append(each).append("\r\n");
+		}
+		cache.append("\t}\r\n");
+		return cache.toString();
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return methodName.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if (o instanceof MethodModel == false)
+		{
+			return false;
+		}
+		MethodModel target = (MethodModel) o;
+		if (accessLevel != target.accessLevel)
+		{
+			return false;
+		}
+		if (methodName.equals(target.methodName) == false)
+		{
+			return false;
+		}
+		if (paramterTypes.length != target.paramterTypes.length)
+		{
+			return false;
+		}
+		for (int i = 0; i < paramterTypes.length; i++)
+		{
+			Class<?> class1 = paramterTypes[i];
+			Class<?> class2 = target.paramterTypes[i];
+			if (class1 != class2)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public MethodModelKey generateKey()
+	{
+		MethodModelKey key = new MethodModelKey();
+		key.accessLevel = accessLevel;
+		key.methodName = methodName;
+		key.paramterTypes = paramterTypes;
+		return key;
+	}
+	
+	/**
+	 * 生成方法的调用字符串，类似xxx($0,$1,$2...)。xxx是方法名
+	 * 
+	 * @return
+	 */
+	public String generateInvoke()
+	{
+		StringCache cache = new StringCache();
+		cache.append(methodName).append('(');
+		for (int i = 0; i < paramterTypes.length; i++)
+		{
+			cache.append("$").append(i).appendComma();
+		}
+		if (cache.isCommaLast())
+		{
+			cache.deleteLast();
+		}
+		cache.append(')');
+		return cache.toString();
+	}
+	
+	public static class MethodModelKey
+	{
+		private AccessLevel	accessLevel;
+		private Class<?>[]	paramterTypes;
+		private String		methodName;
+		
+		public MethodModelKey()
+		{
+		}
+		
+		public MethodModelKey(Method method)
+		{
+			int modifiers = method.getModifiers();
+			if (Modifier.isPublic(modifiers))
+			{
+				accessLevel = AccessLevel.PUBLIC;
+			}
+			else if (Modifier.isPrivate(modifiers))
+			{
+				accessLevel = AccessLevel.PRIVATE;
+			}
+			else if (Modifier.isProtected(modifiers))
+			{
+				accessLevel = AccessLevel.PROTECTED;
+			}
+			methodName = method.getName();
+			paramterTypes = method.getParameterTypes();
+		}
+		
+		@Override
+		public int hashCode()
+		{
+			return methodName.hashCode();
+		}
+		
+		@Override
+		public boolean equals(Object o)
+		{
+			if (o instanceof MethodModel == false)
+			{
+				return false;
+			}
+			MethodModel target = (MethodModel) o;
+			if (accessLevel != target.accessLevel)
+			{
+				return false;
+			}
+			if (methodName.equals(target.methodName) == false)
+			{
+				return false;
+			}
+			if (paramterTypes.length != target.paramterTypes.length)
+			{
+				return false;
+			}
+			for (int i = 0; i < paramterTypes.length; i++)
+			{
+				Class<?> class1 = paramterTypes[i];
+				Class<?> class2 = target.paramterTypes[i];
+				if (class1 != class2)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		public void setAccessLevel(AccessLevel accessLevel)
+		{
+			this.accessLevel = accessLevel;
+		}
+		
+		public void setParamterTypes(Class<?>[] paramterTypes)
+		{
+			this.paramterTypes = paramterTypes;
+		}
+		
+		public void setMethodName(String methodName)
+		{
+			this.methodName = methodName;
+		}
+		
+	}
+	
+	public AccessLevel getAccessLevel()
+	{
+		return accessLevel;
+	}
+	
+	public void setAccessLevel(AccessLevel accessLevel)
+	{
+		this.accessLevel = accessLevel;
+	}
+	
+	public Class<?> getReturnType()
+	{
+		return returnType;
+	}
+	
+	public void setReturnType(Class<?> returnType)
+	{
+		this.returnType = returnType;
+	}
+	
+	public Class<?>[] getParamterTypes()
+	{
+		return paramterTypes;
+	}
+	
+	public void setParamterTypes(Class<?>... paramterTypes)
+	{
+		this.paramterTypes = paramterTypes;
+	}
+	
+	public Class<?>[] getThrowables()
+	{
+		return throwables;
+	}
+	
+	public void setThrowables(Class<?>... throwables)
+	{
+		this.throwables = throwables;
+	}
+	
+	public String getMethodName()
+	{
+		return methodName;
+	}
+	
+	public void setMethodName(String methodName)
+	{
+		this.methodName = methodName;
+	}
+	
+	public String getBody()
+	{
+		return body;
+	}
+	
+	public void setBody(String body)
+	{
+		this.body = body;
+	}
+	
+	public void setParamterFinals(boolean... paramterFinals)
+	{
+		this.paramterFinals = paramterFinals;
+	}
 }
