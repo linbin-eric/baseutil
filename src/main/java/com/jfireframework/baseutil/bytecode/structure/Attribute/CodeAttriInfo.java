e43c0d691205f40e3b51e8f36dd13f8c671abab6
eric <eric@jfire.cn>
2018-09-16 14:45:35 +0800

    删除2个不必要的字段

--- src/main/java/com/jfireframework/baseutil/bytecode/structure/Attribute/CodeAttriInfo.java
+++ src/main/java/com/jfireframework/baseutil/bytecode/structure/Attribute/CodeAttriInfo.java
@@ -12,10 +12,7 @@ public class CodeAttriInfo extends AttributeInfo
     //2字节长度
     private int max_locals;
     private int code_length;
-    private byte[] code;
-    private int exception_table_length;
     private ExceptionHandler[] exceptionHandlers;
-    private int attributes_count;
     private AttributeInfo[] attributeInfos;
 
     public CodeAttriInfo(String name, int length)
@@ -33,15 +30,15 @@ public class CodeAttriInfo extends AttributeInfo
         code_length = ((bytes[counter] & 0xff) << 24)//
                 | ((bytes[counter + 1] & 0xff) << 16)//
                 | ((bytes[counter + 2] & 0xff) << 8)//
-                | ((bytes[counter + 3] & 0xff) << 0);
+                | ((bytes[counter + 3] & 0xff));
         counter += 4;
         //由于目前不做解析，因此忽略掉这部分code数据
         counter += code_length;
-        exception_table_length = ((bytes[counter] & 0xff) << 8) | (bytes[counter + 1] & 0xff);
+        int exception_table_length = ((bytes[counter] & 0xff) << 8) | (bytes[counter + 1] & 0xff);
         counter += 2;
         //目前不解析异常表信息，忽略这部分数据
         counter += exception_table_length * 8;
-        attributes_count = ((bytes[counter] & 0xff) << 8) | (bytes[counter + 1] & 0xff);
+        int attributes_count = ((bytes[counter] & 0xff) << 8) | (bytes[counter + 1] & 0xff);
         counter += 2;
         attributeInfos = new AttributeInfo[attributes_count];
         for (int i = 0; i < attributeInfos.length; i++)
@@ -59,6 +56,6 @@ public class CodeAttriInfo extends AttributeInfo
     @Override
     public String toString()
     {
-        return "CodeAttriInfo{" + "max_stack=" + max_stack + ", max_locals=" + max_locals + ", code_length=" + code_length + ", code=" + Arrays.toString(code) + ", exception_table_length=" + exception_table_length + ", exceptionHandlers=" + Arrays.toString(exceptionHandlers) + ", attributes_count=" + attributes_count + ", attributeInfos=" + Arrays.toString(attributeInfos) + '}';
+        return "CodeAttriInfo{" + "max_stack=" + max_stack + ", max_locals=" + max_locals + ", code_length=" + code_length + ", exceptionHandlers=" + Arrays.toString(exceptionHandlers) + ", attributeInfos=" + Arrays.toString(attributeInfos) + '}';
     }
 }
