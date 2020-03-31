4c8e2c7a79f8ea207117551344378f01e2cde0a8
eric <495561397@qq.com>
2020-03-31 09:55:44 +0800

    重构Copy功能，去掉了不必要的属性描述等。

--- src/test/java/com/jfirer/baseutil/CopyTest.java
+++ src/test/java/com/jfirer/baseutil/CopyTest.java
@@ -1,6 +1,9 @@
 package com.jfirer.baseutil;
 
-import com.jfirer.baseutil.reflect.copy.*;
+import com.jfirer.baseutil.reflect.copy.Copy;
+import com.jfirer.baseutil.reflect.copy.CopyFrom;
+import com.jfirer.baseutil.reflect.copy.CopyIgnore;
+import com.jfirer.baseutil.reflect.copy.CopyTo;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,9 +12,7 @@ public class CopyTest
     @Test
     public void test()
     {
-        Copy<Person, Person> util = new FastCopy<Person, Person>()
-        {
-        };
+        Copy<Person, Person> util = new Copy<>(Person.class, Person.class);
         Person               src  = new Person();
         src.setAge(13);
         src.setSex(true);
@@ -24,9 +25,7 @@ public class CopyTest
     @Test
     public void test_2()
     {
-        Copy<Person, Person2> copy = new FastCopy<CopyTest.Person, CopyTest.Person2>()
-        {
-        };
+        Copy<Person, Person2> copy = new Copy<>(Person.class, Person2.class);
         Person                src  = new Person();
         src.setAge(13);
         src.setSex(true);
@@ -39,9 +38,7 @@ public class CopyTest
     @Test
     public void test_3()
     {
-        Copy<Person, Person> util = new FastCopy<CopyTest.Person, CopyTest.Person>()
-        {
-        };
+        Copy<Person, Person> util = new Copy<>(Person.class, Person.class);
         Person               src  = new Person();
         src.setAge(13);
         src.setSex(true);
@@ -54,9 +51,7 @@ public class CopyTest
     @Test
     public void test_4()
     {
-        Copy<Person, Person2> copy = new FastCopy<CopyTest.Person, CopyTest.Person2>()
-        {
-        };
+        Copy<Person, Person2> copy = new Copy<>(Person.class, Person2.class);
         Person                src  = new Person();
         src.setAge(13);
         src.setSex(true);
@@ -69,16 +64,12 @@ public class CopyTest
     @Test
     public void test_5()
     {
-        Copy<Person, Person3> copy   = new FastCopy<CopyTest.Person, CopyTest.Person3>()
-        {
-        };
+        Copy<Person, Person3> copy   = new Copy<>(Person.class, Person3.class);
         Person                person = new Person();
         person.setSex(true);
         Person3 person3 = copy.copy(person, new Person3());
         Assert.assertTrue(person3.getSex3());
-        Copy<Person2, Person3> copy2   = new FastCopy<CopyTest.Person2, CopyTest.Person3>()
-        {
-        };
+        Copy<Person2, Person3> copy2   = new Copy<>(Person2.class, Person3.class);
         Person2                person2 = new Person2();
         person2.setSex2(true);
         person3 = copy2.copy(person2, new Person3());
@@ -88,9 +79,7 @@ public class CopyTest
     @Test
     public void test_6()
     {
-        Copy<Person, Person4> copy   = new FastCopy<CopyTest.Person, CopyTest.Person4>()
-        {
-        };
+        Copy<Person, Person4> copy   = new Copy<>(Person.class, Person4.class);
         Person                person = new Person();
         person.setSex1(true);
         Person4 person4 = copy.copy(person, new Person4());
@@ -100,9 +89,7 @@ public class CopyTest
     @Test
     public void test_7()
     {
-        Copy<Person, Person5> copy   = new FastCopy<CopyTest.Person, CopyTest.Person5>()
-        {
-        };
+        Copy<Person, Person5> copy   = new Copy<>(Person.class, Person5.class);
         Person                person = new Person();
         person.setSex(true);
         Person5 person5 = new Person5();
