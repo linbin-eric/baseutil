ed4b4368799e8ce7251580c4b796f1f67167d65c
linbin <495561397@qq.com>
2024-09-18 21:02:34 +0800

    迭代 ValueAccessor

--- src/main/java/com/jfirer/baseutil/reflect/CompileValueAccessor.java
+++ src/main/java/com/jfirer/baseutil/reflect/CompileValueAccessor.java
@@ -7,6 +7,7 @@ import com.jfirer.baseutil.smc.compiler.CompileHelper;
 import com.jfirer.baseutil.smc.model.ClassModel;
 import com.jfirer.baseutil.smc.model.MethodModel;
 
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,39 +21,66 @@ public class CompileValueAccessor
         return field.getName().substring(0, 1).toUpperCase() + field.getName().substring(1);
     }
 
-    public static ValueAccessor create(Field field, CompileHelper compileHelper) throws NoSuchMethodException
+    public static ValueAccessor create(Field field, CompileHelper compileHelper) throws NoSuchMethodException, IOException, ClassNotFoundException
     {
         ClassModel classModel = new ClassModel("ValueAccessor_" + field.getName() + "_" + count.getAndIncrement());
         classModel.addInterface(ValueAccessor.class);
-        int    classId    = ReflectUtil.getClassId(field.getType());
-        String methodName = ReflectUtil.isBooleanOrBooleanBox(field.getType()) ? "is" + toMethodName(field) : "get" + toMethodName(field);
-        //这个方法本身必须存在
-        if (ReflectUtil.isBooleanOrBooleanBox(field.getType()))
+        classModel.addImport(field.getDeclaringClass());
+        int    classId       = ReflectUtil.getClassId(field.getType());
+        String getMethodName = ReflectUtil.isBooleanOrBooleanBox(field.getType()) ? "is" + toMethodName(field) : "get" + toMethodName(field);
+        String setMethodName = "set" + toMethodName(field);
+        field.getDeclaringClass().getMethod(getMethodName);
+        field.getDeclaringClass().getMethod(setMethodName, field.getType());
+        if (ReflectUtil.isPrimitive(field.getType()))
         {
-            field.getDeclaringClass().getMethod(methodName);
+            MethodModel methodModel = switch (classId)
+            {
+                case ReflectUtil.PRIMITIVE_BYTE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getByte", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_CHAR -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getChar", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_DOUBLE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getDouble", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_FLOAT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getFloat", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_INT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getInt", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_LONG -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getLong", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_SHORT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getShort", Object.class), classModel);
+                case ReflectUtil.PRIMITIVE_BOOL -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getBoolean", Object.class), classModel);
+                default -> throw new IllegalStateException("Unexpected value: " + classId);
+            };
+            methodModel.setParamterNames("entity");
+            methodModel.setBody("return ("+SmcHelper.getReferenceName(field.getDeclaringClass(), classModel)+  " entity)." + getMethodName + "();");
+            classModel.putMethodModel(methodModel);
+            MethodModel setMethodModel = switch (classId)
+            {
+                case ReflectUtil.PRIMITIVE_BYTE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, byte.class), classModel);
+                case ReflectUtil.PRIMITIVE_CHAR -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, char.class), classModel);
+                case ReflectUtil.PRIMITIVE_DOUBLE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, double.class), classModel);
+                case ReflectUtil.PRIMITIVE_FLOAT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, float.class), classModel);
+                case ReflectUtil.PRIMITIVE_INT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, int.class), classModel);
+                case ReflectUtil.PRIMITIVE_LONG -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, long.class), classModel);
+                case ReflectUtil.PRIMITIVE_SHORT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, short.class), classModel);
+                case ReflectUtil.PRIMITIVE_BOOL -> new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, boolean.class), classModel);
+                default -> throw new IllegalStateException("Unexpected value: " + classId);
+            };
+            setMethodModel.setParamterNames("entity", "value");
+            setMethodModel.setBody("entity." + setMethodName + "(value);");
+            classModel.putMethodModel(methodModel);
         }
-        else
-        {
-            field.getDeclaringClass().getMethod(methodName);
-        }
-        MethodModel methodModel = switch (classId)
-        {
-            case ReflectUtil.PRIMITIVE_BYTE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getByte",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_CHAR -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getChar",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_DOUBLE -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getDouble",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_FLOAT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getFloat",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_INT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getInt",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_LONG -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getLong",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_SHORT -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getShort",Object.class), classModel);
-            case ReflectUtil.PRIMITIVE_BOOL -> new MethodModel(ValueAccessor.class.getDeclaredMethod("getBoolean",Object.class), classModel);
-            default -> throw new IllegalStateException("Unexpected value: " + classId);
-        };
-        methodModel.setParamterNames("entity");
-        methodModel.setBody("return entity." + methodName + "();");
-        classModel.putMethodModel(methodModel);
         MethodModel getMethodModel = new MethodModel(ValueAccessor.class.getDeclaredMethod("get", Object.class), classModel);
         getMethodModel.setParamterNames("entity");
-
+        getMethodModel.setBody("return entity." + getMethodName + "();");
+        classModel.putMethodModel(getMethodModel);
+        MethodModel getRefenceMethodModel = new MethodModel(ValueAccessor.class.getDeclaredMethod("getReference", Object.class), classModel);
+        getRefenceMethodModel.setParamterNames("entity");
+        getRefenceMethodModel.setBody("return entity." + getMethodName + "();");
+        classModel.putMethodModel(getRefenceMethodModel);
+        MethodModel setMethodModel = new MethodModel(ValueAccessor.class.getDeclaredMethod("set", Object.class, Object.class), classModel);
+        setMethodModel.setParamterNames("entity", "value");
+        setMethodModel.setBody("entity." + setMethodName + "(value);");
+        classModel.putMethodModel(setMethodModel);
+        MethodModel setReferenceMethodModel = new MethodModel(ValueAccessor.class.getDeclaredMethod("setReference", Object.class, Object.class), classModel);
+        setReferenceMethodModel.setParamterNames("entity", "value");
+        setReferenceMethodModel.setBody("entity." + setMethodName + "(value);");
+        classModel.putMethodModel(setReferenceMethodModel);
+     compileHelper.compile(classModel);
     }
 
     private static ValueAccessor build(Field field, CompileHelper compileHelper, ClassModel classModel, String getMethodName, Class<?> C1, Class<?> C2)
